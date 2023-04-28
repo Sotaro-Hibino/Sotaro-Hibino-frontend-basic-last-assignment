176503a6e8ab02d4194790f41df527c1c2efe719
Sotaro-Hibino <s.hibino.23.1.765@4nonome.com>
2023-04-28 23:17:58 +0900

    WIP　collaspeに関して

---
+++ src/stories/organisms-header.stories.js
@@ -0,0 +1,4 @@
+import Organisms_Header from "../components/organisms/events/header/header.html";
+import "../components/organisms/events/header/header.scss";
+export default { title: "Organisms/Header" };
+export const Header = () => Organisms_Header;
