8f88fd660bb2be568c85949f22fa99ccd52cdc8e
Sotaro-Hibino <s.hibino.23.1.765@4nonome.com>
2023-05-11 11:32:35 +0900

    organisms-topの作成

---
+++ src/stories/organisms-top.stories.js
@@ -0,0 +1,4 @@
+import Organisms_Top from "../components/organisms/events/top/top.html";
+import "../components/organisms/events/top/top.scss";
+export default { title: "Organisms/Top" };
+export const Top = () => Organisms_Top;
