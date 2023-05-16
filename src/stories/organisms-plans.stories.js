e763008b217b501f0eec5994a782de3e7f81ef3a
Sotaro-Hibino <s.hibino.23.1.765@4nonome.com>
2023-05-16 18:25:02 +0900

    Revert "organisms/plansの作成"
    This reverts commit 0180a7f44dfb8ca4c7c7061f9058d1d2ba48e330.

--- src/stories/organisms-plans.stories.js
+++
@@ -1,4 +0,0 @@
-import Item_Plans from "../components/organisms/plans/plans.html";
-import "../components/organisms/plans/plans.scss";
-export default { title: "Organisms/Plans" };
-export const Plans = () => Item_Plans;
