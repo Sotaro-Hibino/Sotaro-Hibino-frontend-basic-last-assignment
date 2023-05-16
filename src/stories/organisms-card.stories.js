0d18a5738437142fda3e888c999c98ce815ae93c
Sotaro-Hibino <s.hibino.23.1.765@4nonome.com>
2023-05-16 12:20:16 +0900

    organisms/cardの作成

---
+++ src/stories/organisms-card.stories.js
@@ -0,0 +1,6 @@
+import Item_Card from "../components/organisms/events/card/card.html";
+import "../components/organisms/events/card/card.scss";
+
+export default { title: "Organisms/Card" };
+
+export const Card = () => Item_Card;
