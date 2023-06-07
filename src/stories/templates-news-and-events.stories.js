d3a0f7386ffeca9ba936d138483d1afd21dcfa8f
Sotaro-Hibino <s.hibino.23.1.765@4nonome.com>
2023-06-07 15:37:24 +0900

    news-and-eventsの作成

---
+++ src/stories/templates-news-and-events.stories.js
@@ -0,0 +1,4 @@
+import Templates_News from "../components/templates/news-and-events/news-and-events.html";
+import "../components/templates/news-and-events/news-and-events.scss";
+export default { title: "Templates/News" };
+export const News = () => Templates_News;
