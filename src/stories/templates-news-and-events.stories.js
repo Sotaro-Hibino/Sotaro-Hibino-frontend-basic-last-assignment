a463330efe1dda7a67e15f494b5942b7cd599a43
Sotaro-Hibino <s.hibino.23.1.765@4nonome.com>
2023-08-13 15:30:14 +0900

    背景、タイトルの修正

--- src/stories/templates-news-and-events.stories.js
+++ src/stories/templates-news-and-events.stories.js
@@ -1,4 +1,4 @@
-import Templates_News from "../components/templates/news-and-events/news-and-events.html";
+import Templates_NewsAndsEvents from "../components/templates/news-and-events/news-and-events.html";
 import "../components/templates/news-and-events/news-and-events.scss";
-export default { title: "Templates/News" };
-export const News = () => Templates_News;
+export default { title: "Templates/NewsAndsEvents" };
+export const NewsAndsEvents = () => Templates_NewsAndsEvents;
